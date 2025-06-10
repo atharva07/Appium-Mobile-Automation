6b654ec330b94851f4527eb48fae257b70afdca4
atharva07 <atharva.hiwase07@gmail.com>
2025-06-11 02:11:16 +0530

    Drag and Drop

---
+++ src/test/java/mobileappium/DragDropDemo.java
@@ -0,0 +1,27 @@
+package mobileappium;
+
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+
+public class DragDropDemo extends BaseTest {
+
+    @Test
+    public void DragAndDropTest() throws InterruptedException, MalformedURLException {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+        driver.findElement(AppiumBy.accessibilityId("Drag and Drop")).click();
+        WebElement source = driver.findElement(AppiumBy.id("io.appium.android.apis:id/drag_dot_1"));
+
+        ((JavascriptExecutor) driver).executeScript("mobile: dragGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement) source).getId(),
+                "endX", 644,
+                "endY", 586
+        ));
+
+        Thread.sleep(3000);
+    }
+}
