060f7446affe4f9a18981ce5d13cb72153039c34
atharva07 <atharva.hiwase07@gmail.com>
2025-10-19 14:04:13 +0530

    ECommerce TC

---
+++ src/test/java/eCommerce_App/eCommerce_tc_2.java
@@ -0,0 +1,47 @@
+package eCommerce_App;
+
+import io.appium.java_client.AppiumBy;
+import mobileappium.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class eCommerce_tc_2 extends BaseTest {
+    @Test
+    public void OpeningPage() throws InterruptedException {
+        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Atharva Hiwase");
+        driver.hideKeyboard();
+        driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
+        driver.findElement(By.id("android:id/text1")).click();
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));"));
+        driver.findElement(By.xpath("//android.widget.TextView[@text='Argentina']")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+        Thread.sleep(3000);
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Jordan 6 Rings\"));"));
+
+        int productCount = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
+
+        for (int i = 0; i < productCount; i++) {
+            String productName = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText();
+
+            if (productName.equalsIgnoreCase("Jordan 6 Rings")) {
+                driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(i).click();
+            }
+        }
+
+        driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+        Thread.sleep(3000);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.attributeContains(driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
+
+        String lastpageProduct = driver.findElement(By.id("com.androidsample.generalstore:id/productName")).getText();
+        Assert.assertEquals(lastpageProduct, "Jordan 6 Rings");
+    }
+}
--- src/test/java/mobileappium/BaseTest.java
+++ src/test/java/mobileappium/BaseTest.java
@@ -36,7 +36,8 @@ public class BaseTest {
 
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName("MobileAutomationMachine");
-        options.setApp("D://Appium//resources//ApiDemos-debug.apk");
+        options.setApp("D://Appium//resources//General-Store.apk");
+        //options.setApp("D://Appium//resources//ApiDemos-debug.apk");
         // Initialize the Appium driver
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
